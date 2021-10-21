1d67233518b4d510374b2f576203e6e30bdb3277
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-21 09:46:15 +0530

    ok

---
+++ AddressBook/CSVData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVFile
+{
+    public class CSVData
+    {
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string Contact { get; set; }
+        public string City { get; set; }
+        public string Compony { get; set; }
+
+
+
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using System;
+using System.IO;
+
+namespace CSVFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Day28\CopyCSVFile\Vishal2.csv";
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                var record = csv.GetRecords<CSVData>();
+                foreach (CSVData data in record)
+                {
+                    Console.WriteLine(data.Name);
+                    Console.WriteLine(data.Mail);
+                    Console.WriteLine(data.Contact);
+                    Console.WriteLine(data.City);
+                    Console.WriteLine(data.Compony);
+                }
+            }
+
+        }
+    }
+}
