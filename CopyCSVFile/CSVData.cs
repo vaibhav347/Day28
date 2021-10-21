c378689db737496d15a5dd5d4da2f51315955b83
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-21 09:44:19 +0530

    ok

---
+++ CopyCSVFile/CSVData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyCSVFile
+{
+    public class CSVData
+    {
+        public string Name { get; set; }
+        public string Mail { get; set; }
+    }
+}
---
+++ CopyCSVFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using System;
+using System.IO;
+
+namespace CopyCSVFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathSource = @"C:\Day28\CopyCSVFile\Vishal2.csv";
+            string pathDest = @"C:\Day28\CopyCSVFile\Vishal.csv";
+            using (var reader = new StreamReader(pathSource))
+            using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
+
+            {
+                var record = csv.GetRecords<CSVData>();
+                foreach (CSVData data in record)
+                {
+                    Console.WriteLine(data.Name);
+                    Console.WriteLine(data.Mail);
+                }
+
+
+                using (var writer = new StreamWriter(pathDest))
+                using (var csv2 = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+                {
+                    csv2.WriteRecords(record);
+                }
+            }
+
+        }
+    }
+}
